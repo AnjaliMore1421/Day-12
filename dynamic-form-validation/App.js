d6b1738c1c4bb6eb4448c447396ee8be0009575f
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-06 18:32:53 +0530

    Update App.js

--- dynamic-form-validation/App.js
+++ dynamic-form-validation/App.js
@@ -1,8 +1,14 @@
+// Import React library
 import React from "react";
+
+// Import the DynamicFormValidation component
 import DynamicFormValidation from "./DynamicFormValidation";
 
+// Main App component
 function App() {
+  // Render the DynamicFormValidation form component
   return <DynamicFormValidation />;
 }
 
+// Export App component for use in index.js
 export default App;
