ad9416fa29b273d4377442f4eb373124293b2a26
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-06 18:34:41 +0530

    Update App.js

--- controlled-input-form/src/App.js
+++ controlled-input-form/src/App.js
@@ -1,12 +1,18 @@
+// Import React library
 import React from "react";
+
+// Import ControlledForm component
 import ControlledForm from "./ControlledForm";
 
+// Main App component
 function App() {
   return (
     <div>
+      {/* Render the controlled form component */}
       <ControlledForm />
     </div>
   );
 }
 
+// Export App component
 export default App;
