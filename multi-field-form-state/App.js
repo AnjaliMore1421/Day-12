9f31024cbe8c89cf7b6b3d956f83688692b7453d
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-06 18:28:32 +0530

    Update App.js

--- multi-field-form-state/App.js
+++ multi-field-form-state/App.js
@@ -1,8 +1,14 @@
+// Importing React library to create the component
 import React from "react";
+
+// Importing the MultiFieldForm component from the same folder
 import MultiFieldForm from "./MultiFieldForm";
 
+// Main App component
 function App() {
+  // Rendering the MultiFieldForm component
   return <MultiFieldForm />;
 }
 
+// Exporting App component so it can be used in index.js
 export default App;
